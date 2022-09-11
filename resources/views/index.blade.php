8443a21f8dcd00f8fc871991b78237de096a4659
yansem <janchevsem@mail.ru>
2022-09-11 18:56:34 +0300

    bootstrap styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/sass/app.scss'])
     <title>Тестовое задание</title>
 </head>
 <body>
